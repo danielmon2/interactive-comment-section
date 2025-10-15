d826a26f8d47b57d755020eb60e49d35d952d3df
Daniel <dan.nov345@gmail.com>
2025-10-15 15:15:19 +0200

    Add and rename CSS classes in DeleteModal

--- src/DeleteModal/DeleteModal.jsx
+++ src/DeleteModal/DeleteModal.jsx
@@ -17,18 +17,22 @@ const DeleteModal = ({ isOpen, onClose, onDelete }) => {
 
   return (
     <dialog ref={modalRef} className="delete-modal">
-      <h1>Delete comment</h1>
-      <p>
+      <h1 className="delete-modal__h1">Delete comment</h1>
+      <p className="delete-modal__p">
         Are you sure you want to delete this comment? This will remove a comment
         and can't be undone.
       </p>
-      <section>
-        <button autoFocus onClick={onClose} className="btn-base cancel-btn">
+      <section className="delete-modal__btns">
+        <button
+          autoFocus
+          onClick={onClose}
+          className="btn comment__btn delete-modal__btns__cancel-btn"
+        >
           NO, CANCEL
         </button>
         <button
           onClick={() => onDelete(isOpen[1])}
-          className="btn-base delete-btn-modal"
+          className="btn comment__btn delete-modal__btns__delete-btn"
         >
           YES, DELETE
         </button>
