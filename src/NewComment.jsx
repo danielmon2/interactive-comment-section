95515e5f7df074393d7f932e23a31cbd47a05366
Daniel <dan.nov345@gmail.com>
2025-10-15 15:15:15 +0200

    Small CSS refactor. Made a few base classes

--- src/NewComment.jsx
+++ src/NewComment.jsx
@@ -1,9 +1,13 @@
 const NewComment = (props) => {
   return (
-    <div className="new-comment">
-      <img src={props.image.png}></img>
-      <input type="text" placeholder="Add a comment..." />
-      <button>Send</button>
+    <div className="comment-base new-comment">
+      <img className="avatar" src={props.image.png}></img>
+      <input
+        className="new-comment-input"
+        type="text"
+        placeholder="Add a comment..."
+      />
+      <button className="btn-base send-btn">SEND</button>
     </div>
   );
 };
