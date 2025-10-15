79d4d24b0ec033dbb2b0cf2f204fd1f5b67c9fc4
Daniel <dan.nov345@gmail.com>
2025-10-15 15:15:14 +0200

    Only one upvote/downvote allowed

--- src/CommentSection.jsx
+++ src/CommentSection.jsx
@@ -6,7 +6,8 @@ import DeleteModal from "./DeleteModal";
 
 const CommentSection = () => {
   const currentUser = jsonData.currentUser;
-  const [comments, setComments] = useState([...jsonData.comments]);
+  const [comments, setComments] = useState(jsonData.comments);
+  const [isCommentRated, setIsCommentRated] = useState({});
   const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
 
   const handleOpenDeleteModal = () => {
@@ -17,10 +18,37 @@ const CommentSection = () => {
     setDeleteModalOpen(false);
   };
 
+  const calculateNewScore = (id, action, score) => {
+    // Check if user already scored this comment
+    if (id in isCommentRated) {
+      // Check whether it was the same action
+      if (isCommentRated[id] === action) {
+        // If yes - erase it
+        setIsCommentRated((current) => {
+          const newRatings = { ...current };
+          delete newRatings[id];
+          return newRatings;
+        });
+        if (action === "upvote") return score - 1;
+        else return score + 1;
+      } else {
+        // If not - reverse it
+        setIsCommentRated({ ...isCommentRated, [id]: action });
+        if (action === "upvote") return score + 2;
+        else return score - 2;
+      }
+    } else {
+      // Add new action to the list
+      setIsCommentRated({ ...isCommentRated, [id]: action });
+      if (action === "upvote") return score + 1;
+      else return score - 1;
+    }
+  };
+
   const handleScoreChange = (id, action) => {
     comments.forEach((el, index) => {
       if (el.id === id) {
-        const newScore = action === "upvote" ? el.score + 1 : el.score - 1;
+        const newScore = calculateNewScore(id, action, el.score);
         const newComments = [...comments];
         newComments[index] = { ...comments[index], score: newScore };
         setComments(newComments);
@@ -34,9 +62,9 @@ const CommentSection = () => {
             // Objects, arrays even at the top-most layer are treated as nested
             // If you want to change something inside one you have to copy it (if you want to replace it you don't)
             // I make a shallow copy of each layer I go down
-            // Comments array -> specific comment -> all replies of that comment -> specific reply
+            // Comments array -> specific comment object -> array of all replies of that comment -> specific reply object
             // I'm assuming this will be faster then a deep copy, because here you copy only necessary items
-            const newScore = action === "upvote" ? el.score + 1 : el.score - 1;
+            const newScore = calculateNewScore(id, action, el.score);
             const newComments = [...comments];
             newComments[parentIndex] = { ...comments[parentIndex] };
             newComments[parentIndex].replies = [
