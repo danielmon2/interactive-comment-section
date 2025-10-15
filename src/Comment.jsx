f20b040eff6300fe72daccd75d9e1b38bd4849ee
Daniel <dan.nov345@gmail.com>
2025-10-15 15:15:12 +0200

    Moved creating replies to CommentSection

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,41 +1,33 @@
 const Comment = (props) => {
   return (
-    <div className="comment-container">
-      <div className={props.reply === true ? "reply" : "comment"}>
-        <button className="upvote-btn">{props.score}</button>
-        <div>
-          <div>
-            {/* Check if images work */}
-            <img className="avatar" src={props.user.image.png}></img>
-            {/* <img className="avatar" src={props.user.image.webp}></img> */}
-            <p>{props.user.username}</p>
-            <p>{props.createdAt}</p>
-            {props.user.username === props.currentUser ? (
+    <div className={props.reply === true ? "reply" : "comment"}>
+      <button className="upvote-btn">{props.score}</button>
+      <div>
+        <div className="comment-top-row">
+          {/* Check if images work */}
+          <img className="avatar" src={props.user.image.png}></img>
+          {/* <img className="avatar" src={props.user.image.webp}></img> */}
+          <p>{props.user.username}</p>
+          {props.user.username === props.currentUser ? <span>You</span> : ""}
+          <p>{props.createdAt}</p>
+          {props.user.username === props.currentUser ? (
+            <div>
               <button>Delete</button>
-            ) : (
-              ""
-            )}
-            <button>
-              {props.user.username === props.currentUser ? "Edit" : "Reply"}
-            </button>
-          </div>
-          <p>{props.content}</p>
+              <button>Edit</button>
+            </div>
+          ) : (
+            <button>Reply</button>
+          )}
         </div>
+        <p>
+          {props.replyingTo !== undefined ? (
+            <span className="replyingTo">{"@" + props.replyingTo + " "}</span>
+          ) : (
+            ""
+          )}
+          {props.content}
+        </p>
       </div>
-      {props.replies !== undefined
-        ? props.replies.map((el) => (
-            <Comment
-              reply={true}
-              score={el.score}
-              user={el.user}
-              createdAt={el.createdAt}
-              content={el.content}
-              key={el.id}
-              replies={el.replies}
-              currentUser={props.currentUser}
-            />
-          ))
-        : ""}
     </div>
   );
 };
--- src/CommentSection.jsx
+++ src/CommentSection.jsx
@@ -8,16 +8,36 @@ const CommentSection = () => {
   return (
     <div className="comment-section">
       {comments.map((el) => (
-        <Comment
-          reply={false}
-          score={el.score}
-          user={el.user}
-          createdAt={el.createdAt}
-          content={el.content}
-          key={el.id}
-          replies={el.replies}
-          currentUser={currentUser.username}
-        />
+        <div className="comment-chain" key={"comment-chain_" + el.id}>
+          <Comment
+            reply={false}
+            score={el.score}
+            user={el.user}
+            createdAt={el.createdAt}
+            content={el.content}
+            key={el.id}
+            currentUser={currentUser.username}
+          />
+          {el.replies.length !== 0 ? (
+            <div className="reply-container">
+              <div className="line"></div>
+              {el.replies.map((el) => (
+                <Comment
+                  reply={true}
+                  score={el.score}
+                  user={el.user}
+                  createdAt={el.createdAt}
+                  content={el.content}
+                  key={el.id}
+                  replyingTo={el.replyingTo}
+                  currentUser={currentUser.username}
+                />
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       ))}
       <NewComment image={currentUser.image} />
     </div>
