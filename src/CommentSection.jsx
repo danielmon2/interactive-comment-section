900b89c6985ad4a8e964fb9fd82146251b121d6c
Daniel <dan.nov345@gmail.com>
2025-10-15 15:15:14 +0200

    Moved changing score into a different file

--- src/CommentSection.jsx
+++ src/CommentSection.jsx
@@ -3,6 +3,7 @@ import Comment from "./Comment";
 import NewComment from "./NewComment";
 import { useState } from "react";
 import DeleteModal from "./DeleteModal";
+import changeCommentState, { calculateNewScore } from "./ModifyCommentState";
 
 const CommentSection = () => {
   const currentUser = jsonData.currentUser;
@@ -18,68 +19,15 @@ const CommentSection = () => {
     setDeleteModalOpen(false);
   };
 
-  const calculateNewScore = (id, action, score) => {
-    // Check if user already scored this comment
-    if (id in isCommentRated) {
-      // Check whether it was the same action
-      if (isCommentRated[id] === action) {
-        // If yes - erase it
-        setIsCommentRated((current) => {
-          const newRatings = { ...current };
-          delete newRatings[id];
-          return newRatings;
-        });
-        if (action === "upvote") return score - 1;
-        else return score + 1;
-      } else {
-        // If not - reverse it
-        setIsCommentRated({ ...isCommentRated, [id]: action });
-        if (action === "upvote") return score + 2;
-        else return score - 2;
-      }
-    } else {
-      // Add new action to the list
-      setIsCommentRated({ ...isCommentRated, [id]: action });
-      if (action === "upvote") return score + 1;
-      else return score - 1;
-    }
-  };
-
   const handleScoreChange = (id, action) => {
-    comments.forEach((el, index) => {
-      if (el.id === id) {
-        const newScore = calculateNewScore(id, action, el.score);
-        const newComments = [...comments];
-        newComments[index] = { ...comments[index], score: newScore };
-        setComments(newComments);
-        return;
-      }
-      if (el.replies.length !== 0) {
-        const parentIndex = index;
-        el.replies.forEach((el, index) => {
-          if (el.id === id) {
-            // Shallow copy (spread) makes a copy of the top-most layer.
-            // Objects, arrays even at the top-most layer are treated as nested
-            // If you want to change something inside one you have to copy it (if you want to replace it you don't)
-            // I make a shallow copy of each layer I go down
-            // Comments array -> specific comment object -> array of all replies of that comment -> specific reply object
-            // I'm assuming this will be faster then a deep copy, because here you copy only necessary items
-            const newScore = calculateNewScore(id, action, el.score);
-            const newComments = [...comments];
-            newComments[parentIndex] = { ...comments[parentIndex] };
-            newComments[parentIndex].replies = [
-              ...comments[parentIndex].replies,
-            ];
-            newComments[parentIndex].replies[index] = {
-              ...comments[parentIndex].replies[index],
-            };
-            newComments[parentIndex].replies[index].score = newScore;
-            setComments(newComments);
-            return;
-          }
-        });
-      }
-    });
+    const [newRatings, newComments] = changeCommentState(
+      comments,
+      id,
+      action,
+      isCommentRated,
+    );
+    setComments(newComments);
+    setIsCommentRated(newRatings);
   };
 
   return (
@@ -87,13 +35,12 @@ const CommentSection = () => {
       {comments.map((el) => (
         <div className="comment-chain" key={"comment-chain_" + el.id}>
           <Comment
-            score={el.score}
             data={el}
             key={el.id}
             currentUser={currentUser.username}
             onDelete={handleOpenDeleteModal}
             onScoreChange={handleScoreChange}
-            commentRated={isCommentRated[el.id] && isCommentRated[el.id]}
+            commentRating={isCommentRated[el.id]}
           />
           {el.replies.length !== 0 && (
             <div className="reply-container">
@@ -102,14 +49,11 @@ const CommentSection = () => {
                 {el.replies.map((el) => (
                   <Comment
                     key={el.id}
-                    score={el.score}
                     data={el}
                     currentUser={currentUser.username}
                     onDelete={handleOpenDeleteModal}
                     onScoreChange={handleScoreChange}
-                    commentRated={
-                      isCommentRated[el.id] && isCommentRated[el.id]
-                    }
+                    commentRating={isCommentRated[el.id]}
                   />
                 ))}
               </div>
---
+++ src/ModifyCommentState.jsx
@@ -0,0 +1,103 @@
+/*
+  Change Score (search + prev value)
+
+  Delete comment (search)
+
+  Add new comments
+    Add temporary comment (search)
+
+    Replace it with a proper one (search)
+
+  Edit comment
+    Fetch content (search)
+
+    Change content (search)
+
+  id, optional: action, property;
+    search
+      copies the state but doesnt change it
+
+    Decide what to do with the copy
+
+  Selective deep copy func
+*/
+const calculateNewScore = (isCommentRated, id, action, score) => {
+  // Check if user already scored this comment
+  if (id in isCommentRated) {
+    // Check whether it was the same action
+    if (isCommentRated[id] === action) {
+      // If yes - erase it
+      // ...rest when using spread (deletes a value and leaves the rest).
+      // If object key is in a variable, it has to have ": something"
+      const { [id]: removed, ...newRatings } = isCommentRated;
+      if (action === "upvote") return [score - 1, newRatings];
+      else return [score + 1, newRatings];
+    } else {
+      // If not - reverse it
+      const newRatings = { ...isCommentRated, [id]: action };
+      if (action === "upvote") return [score + 2, newRatings];
+      else return [score - 2, newRatings];
+    }
+  } else {
+    // Add new action to the list
+    const newRatings = { ...isCommentRated, [id]: action };
+    if (action === "upvote") return [score + 1, newRatings];
+    else return [score - 1, newRatings];
+  }
+};
+
+const changeCommentState = (comments, id, action, isCommentRated) => {
+  // console.log(comments, typeof comments);
+  // Entries() because it doesn't work without it (if array has objects in it)
+  for (const [index, el] of comments.entries()) {
+    if (el.id === id) {
+      const newComments = [...comments];
+      newComments[index] = { ...comments[index] };
+      if (action === "upvote" || action === "downvote") {
+        const [newScore, newRatings] = calculateNewScore(
+          isCommentRated,
+          id,
+          action,
+          newComments[index].score,
+        );
+        newComments[index].score = newScore;
+        return [newRatings, newComments];
+      }
+      return newComments;
+    }
+    if (el.replies.length !== 0) {
+      const parentIndex = index;
+      const parentEl = el.replies;
+      for (const [index, el] of parentEl.entries()) {
+        if (el.id === id) {
+          // Shallow copy (spread) makes a copy of the top-most layer.
+          // Objects, arrays even at the top-most layer are treated as nested
+          // If you want to change something inside one you have to copy it (if you want to replace it you don't)
+          // I make a shallow copy of each layer I go down
+          // Comments array -> specific comment object -> array of all replies of that comment -> specific reply object
+          // I'm assuming this will be faster then a deep copy, because here you copy only necessary items
+          const newComments = [...comments];
+          newComments[parentIndex] = { ...comments[parentIndex] };
+          newComments[parentIndex].replies = [...comments[parentIndex].replies];
+          newComments[parentIndex].replies[index] = {
+            ...comments[parentIndex].replies[index],
+          };
+          if (action === "upvote" || action === "downvote") {
+            const [newScore, newRatings] = calculateNewScore(
+              isCommentRated,
+              id,
+              action,
+              newComments[parentIndex].replies[index].score,
+            );
+            newComments[parentIndex].replies[index].score = newScore;
+            return [newRatings, newComments];
+          }
+          return newComments;
+        }
+      }
+    }
+  }
+};
+
+export default changeCommentState;
+export { calculateNewScore };
