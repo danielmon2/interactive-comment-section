5f561099c5e1d2804e3eb44dedc37de2048c7948
Daniel <dan.nov345@gmail.com>
2025-10-15 15:15:12 +0200

    Comment refactor. Moved all ifs into functions body

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,31 +1,72 @@
-const Comment = (props) => {
+const Comment = ({
+  score,
+  username,
+  image,
+  createdAt,
+  content,
+  replyingTo,
+  currentUser,
+}) => {
+  let currentUserIndicator = "";
+  let userButtons = (
+    <button className="reply-btn">
+      <img src="/assets/images/icon-reply.svg" />
+      Reply
+    </button>
+  );
+  if (username === currentUser) {
+    // Add current user indicator
+    currentUserIndicator = (
+      <div className="current-user-indicator">
+        <span>you</span>
+      </div>
+    );
+
+    // Use different buttons if the current comment is made by the current user
+    userButtons = (
+      <div className="current-user-btns">
+        <button className="delete-btn">
+          <img src="/assets/images/icon-delete.svg" />
+          Delete
+        </button>
+        <button className="edit-btn">
+          <img src="/assets/images/icon-edit.svg" />
+          Edit
+        </button>
+      </div>
+    );
+  }
+
+  // Add @ if it's a reply
+  let answerTo = "";
+  if (replyingTo !== undefined) {
+    answerTo = (
+      <a href="#" className="replyingTo">
+        {"@" + replyingTo + " "}
+      </a>
+    );
+  }
   return (
-    <div className={props.reply === true ? "reply" : "comment"}>
-      <button className="upvote-btn">{props.score}</button>
+    <div className="comment">
+      <div className="vote-container">
+        <button className="upvote-btn"></button>
+        <div>
+          <span>{score}</span>
+        </div>
+        <button className="downvote-btn"></button>
+      </div>
+
       <div>
         <div className="comment-top-row">
-          {/* Check if images work */}
-          <img className="avatar" src={props.user.image.png}></img>
-          {/* <img className="avatar" src={props.user.image.webp}></img> */}
-          <p>{props.user.username}</p>
-          {props.user.username === props.currentUser ? <span>You</span> : ""}
-          <p>{props.createdAt}</p>
-          {props.user.username === props.currentUser ? (
-            <div>
-              <button>Delete</button>
-              <button>Edit</button>
-            </div>
-          ) : (
-            <button>Reply</button>
-          )}
+          <img className="avatar" src={image.png}></img>
+          <p className="username">{username}</p>
+          {currentUserIndicator}
+          <p className="created-at">{createdAt}</p>
+          {userButtons}
         </div>
         <p>
-          {props.replyingTo !== undefined ? (
-            <span className="replyingTo">{"@" + props.replyingTo + " "}</span>
-          ) : (
-            ""
-          )}
-          {props.content}
+          {answerTo}
+          {content}
         </p>
       </div>
     </div>
